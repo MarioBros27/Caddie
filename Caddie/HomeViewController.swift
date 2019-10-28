b459e005d6f2866c009033348616ef8c1d0c23e4
Mario <a01187827@itesm.mx>
2019-10-28 12:26:59 -0700

    initialized

---
+++ Caddie/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Caddie
+//
+//  Created by Andres on 10/28/19.
+//  Copyright © 2019 Andres. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Caddie/PlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerCellTableViewCell.swift
+//  Caddie
+//
+//  Created by Andres on 10/28/19.
+//  Copyright © 2019 Andres. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
