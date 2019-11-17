04a4fa090d4b41e75d9aba98b8a32f4fa7623056
Mario <a01187827@itesm.mx>
2019-11-17 11:07:16 -0700

    created data core model

---
+++ Caddie/DataControllers/PlayerDataController.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerDataController.swift
+//  Caddie
+//
+//  Created by Andres on 11/17/19.
+//  Copyright © 2019 Andres. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class PlayerDataController: NSObject {
+
+}
