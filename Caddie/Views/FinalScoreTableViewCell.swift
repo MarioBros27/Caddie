71dc10b9776a552f678e33f8adef9438a4fa4c9f
Mario <a01187827@itesm.mx>
2019-11-20 14:43:09 -0700

    added final score view

---
+++ Caddie/Views/FinalScoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FinalScoreTableViewCell.swift
+//  Caddie
+//
+//  Created by Andres on 11/20/19.
+//  Copyright © 2019 Andres. All rights reserved.
+//
+
+import UIKit
+
+class FinalScoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
